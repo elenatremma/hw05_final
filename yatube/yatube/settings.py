50bec8e58a626e119b879eb870d27fbc9c965de3
Elena Trembovelskaya <tremma@mail.ru>
2023-01-29 17:23:12 +0700

    Spr6_hw_Trembovelskaya_fin

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2o!z1-o-l2fetrrgkp8%fvp$$67p6+lxu6r&#1@ru=@ad)e)hw'
+# SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
